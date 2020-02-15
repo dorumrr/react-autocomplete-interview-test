5d51fb9ce258aa02e53157ceb7b47d72e7bdea0a
Doru Moraru <doru.moraru@gmail.com>
2020-02-15 02:25:30 +0000

    removed useless condition

--- server/api.js
+++ server/api.js
@@ -5,11 +5,6 @@ const cors = require('cors');
 const db = require('./mockdb.json')
 app.use(cors({origin: '*', optionsSuccessStatus: 200}));
 app.get('/', (req, res) => {
-  if(!req.params.q) {
-    return res.json({
-      _success: false
-    });
-  }
   res.json({
     _success: true,
     data: db.reduce((acc, val) => [...acc, Object.keys(val).map(prefix => val[prefix].item[0].name)], [])[0].sort()
